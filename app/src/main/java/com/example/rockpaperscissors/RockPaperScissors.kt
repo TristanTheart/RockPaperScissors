6b5d2114bc3b30bd192419717216c6e5fe3cf325
wishfully28 <thewishfulthinker28@gmail.com>
2024-01-11 16:51:00 +0200

    Rock Paper Scissor Game
    created basic rock paper scissor game using everything I've learned up to this point.

---
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -0,0 +1,54 @@
+package com.example.rockpaperscissors
+
+fun main(){
+    var computerChoice = ""
+    var playerChoice  = ""
+    println("Rock, Paper or Scissors? Enter your choice.")
+    playerChoice = readln().lowercase()
+
+    var randomNumber = (1..3).random()
+
+    val rock = 1
+    val paper = 2
+    val scissors = 3
+
+    if (randomNumber == 1){
+        computerChoice = "rock"
+    } else if (randomNumber == 2){
+        computerChoice = "paper"
+    } else if(randomNumber == 3){
+        computerChoice = "scissors"
+    }
+    if (playerChoice == "rock" &&computerChoice == "paper"){
+       println(computerChoice)
+        println("You picked $playerChoice and I picked $computerChoice,you lose")
+    }else if(playerChoice == "rock" &&computerChoice == "scissors"){
+        println(computerChoice)
+        println("You picked $playerChoice and I picked $computerChoice, you win!")
+    } else if (playerChoice == "rock" &&computerChoice == "rock"){
+        println(computerChoice)
+        println("We both picked rock, its a draw :(")
+
+
+         } else if(playerChoice == "paper" &&computerChoice == "rock"){
+            println(computerChoice)
+            println("You picked $playerChoice and I picked $computerChoice, you win!")
+        } else if(playerChoice == "paper" &&computerChoice == "paper"){
+            println(computerChoice)
+            println("We both picked paper, its a draw :(")
+        } else if (playerChoice == "paper" &&computerChoice == "scissors"){
+            println(computerChoice)
+            println("You picked $playerChoice and I picked $computerChoice, you lose")
+
+
+            } else if(playerChoice == "scissors" &&computerChoice == "rock"){
+                println(computerChoice)
+                println("You picked $playerChoice and I picked $computerChoice, you lose")
+            } else if (playerChoice == "scissors" &&computerChoice == "paper"){
+                println(computerChoice)
+                println("You picked $playerChoice and I picked $computerChoice, you win!")
+            } else if (playerChoice == "scissors" &&computerChoice == "scissors"){
+                println(computerChoice)
+                println("we both picked scissors, its a draw")
+    }
+}
